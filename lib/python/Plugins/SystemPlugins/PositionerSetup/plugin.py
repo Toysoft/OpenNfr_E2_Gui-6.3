3216080e52cb2ad4f13cb3c5e3c88a16d63d4ffb
schleichdi2 <super-harry@gmx.at>
2017-04-10 17:19:23 +0200

    fix oe

--- lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
+++ lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
@@ -120,7 +120,7 @@ class PositionerSetup(Screen):
 			cur.get("pilot", eDVBFrontendParametersSatellite.Pilot_Unknown),
 			cur.get("is_id", 0),
 			cur.get("pls_mode", eDVBFrontendParametersSatellite.PLS_Root),
-			cur.get("pls_code", 0))
+			cur.get("pls_code", 1))
 
 		self.tuner.tune(tp)
 		self.isMoving = False
@@ -1066,7 +1066,7 @@ class TunerScreen(ConfigListScreen, Screen):
 		self.fe_data = fe_data
 		Screen.__init__(self, session)
 		ConfigListScreen.__init__(self, None)
-		self.createConfig()
+		self.createConfig(fe_data)
 		self.initialSetup()
 		self.createSetup()
 		self.tuning.sat.addNotifier(self.tuningSatChanged)
@@ -1079,7 +1079,7 @@ class TunerScreen(ConfigListScreen, Screen):
 			"cancel": self.keyCancel,
 		}, -2)
 
-	def createConfig(self):
+	def createConfig(self, frontendData):
 		satlist = nimmanager.getRotorSatListForNim(self.feid)
 		orb_pos = self.fe_data.get("orbital_position", None)
 		orb_pos_str = str(orb_pos)
@@ -1120,7 +1120,7 @@ class TunerScreen(ConfigListScreen, Screen):
 				defaultSat["pilot"] = frontendData.get("pilot", eDVBFrontendParametersSatellite.Pilot_Unknown)
 				defaultSat["is_id"] = frontendData.get("is_id", 0)
 				defaultSat["pls_mode"] = frontendData.get("pls_mode", eDVBFrontendParametersSatellite.PLS_Root)
-				defaultSat["pls_code"] = frontendData.get("pls_code", 0)
+				defaultSat["pls_code"] = frontendData.get("pls_code", 1)
 			else:
 				defaultSat["fec"] = frontendData.get("fec_inner", eDVBFrontendParametersSatellite.FEC_Auto)
 			defaultSat["modulation"] = frontendData.get("modulation", eDVBFrontendParametersSatellite.Modulation_QPSK)
@@ -1178,7 +1178,7 @@ class TunerScreen(ConfigListScreen, Screen):
 			(eDVBFrontendParametersSatellite.PLS_Root, _("Root")),
 			(eDVBFrontendParametersSatellite.PLS_Gold, _("Gold")),
 			(eDVBFrontendParametersSatellite.PLS_Combo, _("Combo"))])
-		self.scan_sat.pls_code = ConfigInteger(default = defaultSat.get("pls_code",0), limits = (0, 262142))
+		self.scan_sat.pls_code = ConfigInteger(default = defaultSat.get("pls_code",1), limits = (0, 262142))
 
 	def initialSetup(self):
 		currtp = self.transponderToString([None, self.scan_sat.frequency.value, self.scan_sat.symbolrate.value, self.scan_sat.polarization.value])
