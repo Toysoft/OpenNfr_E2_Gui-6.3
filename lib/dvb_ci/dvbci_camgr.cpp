7c131d8f0b0871acc9db96ff0a3d4c02f022f275
schleichdi2 <super-harry@gmx.at>
2017-11-28 19:48:23 +0100

    fix oe

--- lib/dvb_ci/dvbci_camgr.cpp
+++ lib/dvb_ci/dvbci_camgr.cpp
@@ -19,7 +19,6 @@ int eDVBCICAManagerSession::receivedAPDU(const unsigned char *tag, const void *d
 	eDebugNoNewLine("[CI CA] SESSION(%d)/CA %02x %02x %02x: ", session_nb, tag[0], tag[1],tag[2]);
 	for (int i=0; i<len; i++)
 		eDebugNoNewLine("%02x ", ((const unsigned char*)data)[i]);
-	eDebug("");
 
 	if ((tag[0]==0x9f) && (tag[1]==0x80))
 	{
