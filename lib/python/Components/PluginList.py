593464b5b669069747168aed0db4ea9e7fe32909
schleichdi <schleichdi2@users.noreply.github.com>
2018-01-12 11:41:29 +0100

    Update PluginList.py

--- lib/python/Components/PluginList.py
+++ lib/python/Components/PluginList.py
@@ -7,7 +7,7 @@ from Components.config import config
 from enigma import eListboxPythonMultiContent, gFont
 from Tools.LoadPixmap import LoadPixmap
 
-def PluginEntryComponent(plugin, width=440):
+def PluginEntryComponent(plugin, width=900):
 	if plugin.icon is None:
 		png = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "icons/plugin.png"))
 	else:
@@ -15,9 +15,9 @@ def PluginEntryComponent(plugin, width=440):
 	if getDesktop(0).size().width() == 1920:
 	    return [
 		plugin,
-		MultiContentEntryText(pos=(170, 0), size=(width-120, 31), font=0, text=plugin.name),
-		MultiContentEntryText(pos=(210, 32), size=(width-120, 26), font=1, text=plugin.description),
-		MultiContentEntryPixmapAlphaTest(pos=(0, 0), size=(150, 60), png = png)
+		MultiContentEntryText(pos=(170, 5), size=(width-120, 34), font=0, text=plugin.name),
+		MultiContentEntryText(pos=(170, 37), size=(width-120, 28), font=1, text=plugin.description),
+		MultiContentEntryPixmapAlphaTest(pos=(0, 5), size=(150, 60), png = png)
 	]
 
 	if getDesktop(0).size().width() == 1280:
@@ -32,8 +32,8 @@ def PluginCategoryComponent(name, png, width=440):
 	if getDesktop(0).size().width() == 1920:
 	    return [
 		name,
-		MultiContentEntryText(pos=(80, 10), size=(width-80, 32), font=0, text=name),
-		MultiContentEntryPixmapAlphaTest(pos=(10, 5), size=(60, 50), png = png)
+		MultiContentEntryText(pos=(80, 15), size=(width-80, 34), font=0, text=name),
+		MultiContentEntryPixmapAlphaTest(pos=(10, 10), size=(60, 50), png = png)
 	]
         else:
 	   return [
@@ -57,9 +57,9 @@ def PluginDownloadComponent(plugin, name, version=None, width=440):
 	if getDesktop(0).size().width() == 1920:		
 	   return [
 		plugin,
-		MultiContentEntryText(pos=(80, 0), size=(width-80, 32), font=0, text=name),
-		MultiContentEntryText(pos=(120, 32), size=(width-80, 26), font=1, text=plugin.description),
-		MultiContentEntryPixmapAlphaTest(pos=(10, 0), size=(60, 50), png = png)
+		MultiContentEntryText(pos=(80, 5), size=(width-80, 34), font=0, text=name),
+		MultiContentEntryText(pos=(80, 37), size=(width-80, 28), font=1, text=plugin.description),
+		MultiContentEntryPixmapAlphaTest(pos=(10, 10), size=(60, 50), png = png)
 	]
 	else:
 	   return [
@@ -75,9 +75,9 @@ class PluginList(MenuList):
 	def __init__(self, list, enableWrapAround=True):
 		if getDesktop(0).size().width() == 1920:
 				MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
-				self.l.setFont(0, gFont("Regular", 26))
+				self.l.setFont(0, gFont("Regular", 30))
 				self.l.setFont(1, gFont("Regular", 22))
-				self.l.setItemHeight(60)
+				self.l.setItemHeight(70)
 		else:
 			MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
 			self.l.setFont(0, gFont("Regular", 20))
