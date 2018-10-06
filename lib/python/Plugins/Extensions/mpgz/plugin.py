9036934e39403fb0788332114d5fb6c62b414d0f
schleichdi2 <super-harry@gmx.at>
2018-10-06 11:00:10 +0200

    update

--- lib/python/Plugins/Extensions/mpgz/plugin.py
+++ lib/python/Plugins/Extensions/mpgz/plugin.py
@@ -39,7 +39,7 @@
 from update import *
 
 config.mpgz = ConfigSubsection()
-config.mpgz.version = NoSave(ConfigText(default="2018092701"))
+config.mpgz.version = NoSave(ConfigText(default="2018100401"))
 
 def autostart(reason, session=None, **kwargs):
 	if reason == 0:
